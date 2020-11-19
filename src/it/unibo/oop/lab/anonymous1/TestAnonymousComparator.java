e43f0188374302510183bc7842ba597f26163447
Marsild <marsild.spahiu@studio.unibo.it>
2020-11-19 22:51:06 +0100

    fine esercizio 1

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab.anonymous1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUser;
@@ -75,6 +77,13 @@ public final class TestAnonymousComparator {
          * REFER TO LESSON 13-Advanced-Mechanisms.pdf, slide 41
          */
         // TODO
+        Collections.sort(denzelUsers, new Comparator<User>() {
+
+			public int compare(User o1, User o2) {
+				return o1.getAge() - o2.getAge();
+			}
+        	
+        });
         /*
          * expected Result
          */
@@ -105,7 +114,13 @@ public final class TestAnonymousComparator {
          * NOTE: in order to sort a list think about a method of the utility
          * class Collections
          */
-        // TODO
+        Collections.sort(rossiUsers, new Comparator<User>() {
+
+			public int compare(User o1, User o2) {
+				return o2.getAge() - o1.getAge();
+			}
+        	
+        });
         /*
          * expected Result
          */
